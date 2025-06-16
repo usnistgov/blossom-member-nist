d1fe6ffd8b9c0c12852a075a7156f488610c30c3
Cousin <dmitry.cousin@nist.gov>
2025-06-16 19:41:37 -0400

    feat(make): fixed gen-profile invocation
    - added pre-assigned make-variables for the profile generation pre-invocation
    - added informational messages for easier stage understanding
    - switched the tok.sh messaging to be configurable
    - went all the way into python profile generation logic, but still needs work

--- util/aws_okta_test.sh
+++ util/aws_okta_test.sh
@@ -3,25 +3,47 @@
 # Tests AWS CLI for valid credentials and reauthenticates if credentials are
 # invalid or expired.
 # This script also manages the creation of a Python virtual environment.
-echo "In $(basename ${BASH_SOURCE[0]}) script in $( dirname ${BASH_SOURCE[0]} ) dir"
+debug(){
+  if [ -v IS_DEBUGGING ]; then
+    echo "$1"
+  fi
+}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --debug|-d)
+      export IS_DEBUGGING="${2}"
+      shift
+      ;;
+    *)
+      # printf "Error: Incorrect Parameter [${1} ${2}]\n"
+      ### return 1
+      shift
+      ;;
+  esac
+  shift
+done
+
+debug "In $(basename ${BASH_SOURCE[0]}) script in $( dirname ${BASH_SOURCE[0]} ) dir"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 source "${SCRIPT_DIR}/aws_okta_util.sh"
-echo "  1: Verifying that Python/Python3 is installed in PATH"
+debug "  1: Verifying that Python/Python3 is installed in PATH"
+
 # Verify that Python3 is installed
 if ! [ -x "$(command -v python3)" ]; then
   msg 'Error: Python (python3) is not in the PATH, is it installed?'
   exit 1
 fi
 
-echo "  2: Verifying that AWS-CLI is installed and is in PATH"
+debug "  2: Verifying that AWS-CLI is installed and is in PATH"
 # Verify that AWS-CLI is installed
 if ! [ -x "$(command -v aws)" ]; then
   msg 'Error: AWS-CLI (aws) is not in the PATH, is it installed?'
   exit 1
 fi
 
-echo "  3: Verifying that OKTA-AWS-CLI is installed and is in PATH"
+debug "  3: Verifying that OKTA-AWS-CLI is installed and is in PATH"
 # Verify that OKTA-AWS-CLI is installed
 if ! [ -x "$(command -v okta-aws-cli)" ]; then
   msg 'Error: OKTA-AWS-CLI (okta-aws-cli) is not in the PATH, is it installed?'
@@ -30,22 +52,22 @@ if ! [ -x "$(command -v okta-aws-cli)" ]; then
   exit 1
 fi
 
-echo "  4: Checking if there is a currently active logged-in user"
+debug "  4: Checking if there is a currently active logged-in user"
 if aws sts get-caller-identity --profile=$AWS_PROFILE > /dev/null 2>&1; then
     # Credentials are valid, no need to continue
     exit 0
 fi
 
-echo "  5: Setting everything for the python virtual environment"
+debug "  5: Setting everything for the python virtual environment"
 # Reuse function from ./util/util-common.bash
 test_setup_venv
 
-echo "  6: Show environment context"
+debug "  6: Show environment context"
 show_env_info "File: $0 at Line: $LINENO"
-echo "$SCRIPT_DIR=${SCRIPT_DIR}"
+debug "$SCRIPT_DIR=${SCRIPT_DIR}"
 # Run shell subprocess
+debug "  7: Activate virtual environment and run login "
 (
-  echo "  7: Activate virtual environment and run login "
   cd "$SCRIPT_DIR"
 
   source ./venv/bin/activate
